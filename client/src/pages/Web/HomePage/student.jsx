06b76aa8c624be32fb71e4b832648f6c227cd4a4
Lucid271003 <minhtc11b4@gmail.com>
2025-03-11 20:57:00 +0700

    initiate dashboard of tutor and student

---
+++ client/src/pages/Web/HomePage/student.jsx
@@ -0,0 +1,38 @@
+// import {
+//     StudentList,
+//     BlogList,
+//     FileList,
+//     ScheduleList
+// } from "./section";
+
+import { PostList } from "./sections/PostList";
+
+import "./homepage.css";
+
+export const HomePage = () => {
+  return (
+    <main className="homepage">
+      <section>
+        <h2>List Tutors</h2>
+        {/* <StudentList /> */}
+      </section>
+
+      <section>
+        <h2>List Posts</h2>
+        <PostList />
+      </section>
+
+      <section>
+        <h2>List File</h2>
+        {/* <FileList /> */}
+      </section>
+
+      <section>
+        <h2>List Schedule</h2>
+        {/* <ScheduleList /> */}
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
