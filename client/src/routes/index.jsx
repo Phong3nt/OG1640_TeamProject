b8e5162f78386bf05e864a97bfbc388cf71ab201
Nguyen Thanh Phong <phongntgcd210422@fpt.edu.vn>
2025-04-03 16:17:47 +0700

    fix layout

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -9,7 +9,6 @@ import ProfilePage from "../pages/Web/ProfilePage/index";
 
 const ProtectedRoute = ({ children }) => {
   const user = JSON.parse(localStorage.getItem("user"));
-
   return user ? children : <Navigate to="/login" replace />;
 };
 
@@ -19,60 +18,62 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
-
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/blog" element={<BlogPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
 
-      <Route
-        path="/dashboard"
-        element={
-          user?.role ? (
-            <Navigate to={`/dashboard/${user.role}`} replace />
-          ) : (
-            <Navigate to="/login" replace />
-          )
-        }
-      />
+      <Route element={<Layout />}>
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
 
-      <Route
-        path="/dashboard/student"
-        element={
-          <ProtectedRoute>
-            {user?.role === "student" ? (
-              <StudentHomePage />
+        <Route
+          path="/dashboard"
+          element={
+            user?.role ? (
+              <Navigate to={`/dashboard/${user.role}`} replace />
             ) : (
               <Navigate to="/login" replace />
-            )}
-          </ProtectedRoute>
-        }
-      />
+            )
+          }
+        />
 
-      <Route
-        path="/dashboard/tutor"
-        element={
-          <ProtectedRoute>
-            {user?.role === "tutor" ? (
-              <TutorHomePage />
-            ) : (
-              <Navigate to="/login" replace />
-            )}
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/dashboard/student"
+          element={
+            <ProtectedRoute>
+              {user?.role === "student" ? (
+                <StudentHomePage />
+              ) : (
+                <Navigate to="/login" replace />
+              )}
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/dashboard/staff"
-        element={
-          <ProtectedRoute>
-            {user?.role === "staff" ? (
-              <StaffDashboard />
-            ) : (
-              <Navigate to="/login" replace />
-            )}
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/dashboard/tutor"
+          element={
+            <ProtectedRoute>
+              {user?.role === "tutor" ? (
+                <TutorHomePage />
+              ) : (
+                <Navigate to="/login" replace />
+              )}
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard/staff"
+          element={
+            <ProtectedRoute>
+              {user?.role === "staff" ? (
+                <StaffDashboard />
+              ) : (
+                <Navigate to="/login" replace />
+              )}
+            </ProtectedRoute>
+          }
+        />
+      </Route>
 
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
